30fc935c774db4cb95fe4f8d5f9eb72d46ca1c4a
Med Amine <aminedahmen14@gmail.com>
2023-02-14 11:39:22 +0100

    first commit

---
+++ src/Controller/UserController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Controller;
+
+use App\Repository\UserRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Routing\Annotation\Route;
+
+class UserController extends AbstractController
+{
+    #[Route('/user', name: 'app_user')]
+    public function index(): JsonResponse
+    {
+        return $this->json(["message" => "hello from symfony"]);
+    }
+}
